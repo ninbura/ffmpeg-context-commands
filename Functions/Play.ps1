bab48a8516f71f0bd346cba03e5e0b336365af10
TheNimble1 <gabriel@theagnda.com>
2020-04-01 17:45:19 -0600

    Increased visibility of ffplay controls.

--- Functions/Play.ps1
+++ Functions/Play.ps1
@@ -5,24 +5,24 @@ param (
 Import-Module -Name "$(Split-Path $PSScriptRoot -Parent)\Scripts\Joint Functions.ps1"
 
 function PrintControls(){
-    Write-Host "FFplay controls:"
-    Write-Host "[q, ESC] = Quit."
-    Write-Host "[f] = Toggle full screen."
-    Write-Host "[p, SPC] = Pause"
-    Write-Host "[m] = Toggle mute."
-    Write-Host "[9, 0] = Decrease and increase volume respectively."
-    Write-Host "[/, *] = Decrease and increase volume respectively."
-    Write-Host "[a] = Cycle audio channel in the current program."
-    Write-Host "[v] = Cycle video channel."
-    Write-Host "[t] = Cycle subtitle channel in the current program."
-    Write-Host "[c] = Cycle program."
-    Write-Host "[w] = Cycle video filters or show modes."
-    Write-Host "[s] = Step to the next frame."
-    Write-Host "[left/right] = Seek backward/forward 10 seconds."
-    Write-Host "[down/up] = Seek backward/forward 1 minute."
-    Write-Host "[page down/page up] = Seek to the previous/next chapter. or if there are no chapters Seek backward/forward 10 minutes."
-    Write-Host "[right mouse click] = Seek to percentage in file corresponding to fraction of width."
-    Write-Host "[left mouse double-click] = Toggle full screen.`n"
+    Write-Host "FFplay controls:" -ForegroundColor Yellow
+    Write-Host "[q, ESC] = Quit." -ForegroundColor Yellow
+    Write-Host "[f] = Toggle full screen." -ForegroundColor Yellow
+    Write-Host "[p, SPC] = Pause" -ForegroundColor Yellow
+    Write-Host "[m] = Toggle mute." -ForegroundColor Yellow
+    Write-Host "[9, 0] = Decrease and increase volume respectively." -ForegroundColor Yellow
+    Write-Host "[/, *] = Decrease and increase volume respectively." -ForegroundColor Yellow
+    Write-Host "[a] = Cycle audio channel in the current program." -ForegroundColor Yellow
+    Write-Host "[v] = Cycle video channel." -ForegroundColor Yellow
+    Write-Host "[t] = Cycle subtitle channel in the current program." -ForegroundColor Yellow
+    Write-Host "[c] = Cycle program." -ForegroundColor Yellow
+    Write-Host "[w] = Cycle video filters or show modes." -ForegroundColor Yellow
+    Write-Host "[s] = Step to the next frame." -ForegroundColor Yellow
+    Write-Host "[left/right] = Seek backward/forward 10 seconds." -ForegroundColor Yellow
+    Write-Host "[down/up] = Seek backward/forward 1 minute." -ForegroundColor Yellow
+    Write-Host "[page down/page up] = Seek to the previous/next chapter. or if there are no chapters Seek backward/forward 10 minutes." -ForegroundColor Yellow
+    Write-Host "[right mouse click] = Seek to percentage in file corresponding to fraction of width." -ForegroundColor Yellow
+    Write-Host "[left mouse double-click] = Toggle full screen.`n" -ForegroundColor Yellow
 }
 
 Write-Host "FFplay is starting...`n"
