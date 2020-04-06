4e9bf85787f6192ec08546d45e50177cbb303ee7
TheNimble1 <gabriel@theagnda.com>
2020-04-06 17:55:45 -0600

    Revamped auto update functionality.

--- Scripts/Setup Functions.ps1
+++ Scripts/Setup Functions.ps1
@@ -108,7 +108,7 @@ function UpdateFiles($updateBool, $relativePath){
         if(Test-Path -path "$relativePath\.git"){
             while($true){
                 Write-Host "Would you like to update files or delete existing files and then redownload them? (note: if update doesn't work opt for deletion and re-download)"
-                $userConfirmation = Read-Host "Enter `"u`" to update files (mutch faster) without deletion or `"d`" to first delete files and then redownload them [u/d]"
+                $userConfirmation = Read-Host "Enter `"u`" to update files (much faster) without deletion or `"d`" to first delete files and then redownload them [u/d]"
                 Write-Host ""
 
                 if($userConfirmation -match "^d$|^u$"){
